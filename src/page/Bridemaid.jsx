3bb31370f04bf84327f43c1e021580b58654a252
Maulana Rahman Nur <maulanarahmannur67@gmail.com>
2022-02-13 01:58:17 +0700

    update

--- src/page/Bridemaid.jsx
+++ src/page/Bridemaid.jsx
@@ -1,82 +1,38 @@
-import React from 'react';
-import { Carousel } from 'react-carousel-minimal';
+import React, { useEffect, useRef } from "react";
+// import { Carousel } from 'react-carousel-minimal';
+import { Rerousel } from 'rerousel';
 
 import test from '../assets/images/face-evita.jpg';
 
 function Bridemaid() {
 
-  const data = [
-    {
-      image: test,
-      caption: `<div>
-                  Evita
-                  <br/>
-                  Mungil
-                </div>`
-    },
-    {
-      image: test,
-      caption: `<div>
-                  Evita
-                  <br/>
-                  Lagi
-                </div>`
-    },
-    {
-      image: test,
-      caption: `<div>
-                  Evita
-                  <br/>
-                  Wooooooooo
-                </div>`
-    }
-  ];
+  const ref = useRef(null);
 
-  const captionStyle = {
-    fontSize: '2em',
-    fontWeight: 'bold',
-  }
-  const slideNumberStyle = {
-    fontSize: '20px',
-    fontWeight: 'bold',
-  }
+  // const Item = `
+  //   display: flex;
+  //   justify-content: center;
+  //   align-items: center;
+  //   width: calc(100%/2);
+  //   height: 100px;
+  //   font-family: Signika;
+  //   font-weight: bold;
+  //   font-size: 1.5em;
+  //   border: solid 1px black;
+  //   background-color: #61DAFB;
+    
+  //   @media(max-width: 1150px) {
+  //       width: 100%
+  //   }
+  // `;
 
   return (
-    <div className="App">
-      <div style={{ textAlign: "center" }}>
-        <h2>React Carousel Minimal</h2>
-        <p>Easy to use, responsive and customizable carousel component for React Projects.</p>
-        <div style={{
-          padding: "80px 60px"
-        }}>
-          <Carousel
-            data={data}
-            time={1000}
-            width="850px"
-            height="500px"
-            captionStyle={captionStyle}
-            radius="10px"
-            slideNumber={true}
-            slideNumberStyle={slideNumberStyle}
-            captionPosition="bottom"
-            // automatic={true}
-            dots={true}
-            pauseIconColor="white"
-            pauseIconSize="40px"
-            slideBackgroundColor="darkgrey"
-            slideImageFit="cover"
-            thumbnails={true}
-            thumbnailWidth="100px"
-            style={{
-              textAlign: "center",
-              maxWidth: "850px",
-              maxHeight: "500px",
-              margin: "40px auto",
-            }}
-          />
-        </div>
-      </div>
-    </div>
+    <Rerousel itemRef={ref}>
+      <div className="carousel-briedsmaid" ref={ref}>1</div>
+      <div className="carousel-briedsmaid">2</div>
+      <div className="carousel-briedsmaid">3</div>
+      <div className="carousel-briedsmaid">4</div>
+      <div className="carousel-briedsmaid">5</div>
+    </Rerousel>
   );
 }
 
--- src/page/Guestbook.jsx
+++ src/page/Guestbook.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
 export default function Guestbook() {
-  return <div>GUEST BOOK</div>;
+  return (
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-6'>
+          <div class="input-group">
+              <label class="control-label">Username</label>
+              <input type="text" class="form-control form-borderless" placeholder="Username" />
+              <span className='prefix-icon'>
+                <i class="fa-solid fa-user"></i>
+              </span>
+          </div>
+        </div>
+        <div className='col-md-6'>
+          <div id='guest-book'>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
