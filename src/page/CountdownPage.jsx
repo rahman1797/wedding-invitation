7626eb44e01988feeeff96385005448b7ea5e32a
Maulana Rahman Nur <maulanarahmannur67@gmail.com>
2022-02-26 18:32:54 +0700

    update dave date

--- src/page/CountdownPage.jsx
+++ src/page/CountdownPage.jsx
@@ -36,7 +36,7 @@ export default class CountdownPage extends Component {
             <Countdown date={new Date(2022,2,13)} renderer={renderer} />
           </div>
 
-          <a target="_blank" rel="noreferrer" href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=MDE1ZXFubWkxM28zOGM5bm9raG00dWk5YW4gbWF1bGFuYXJhaG1hbm51cjY3QG0&amp;tmsrc=maulanarahmannur67%40gmail.com" className="btn btn-default btn-sm mt-5">Save The Date</a>
+          <a target="_blank" rel="noreferrer" href="https://www.google.com/calendar/render?action=TEMPLATE&text=EvitaRahmanWedding&dates=20220313T080000/20220313T100000" className="btn btn-default btn-sm mt-5">Save The Date</a>
         </div>
       </div>
     )
